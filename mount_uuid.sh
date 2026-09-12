3b41666e0123004b0203c4840033827ecd6bed7c
Brundha S <brundharvs@gmail.com>
2026-09-12 16:57:20 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -6,23 +6,13 @@
 # Roll Number:1U24IT135
 # ==========================================
 
-
 # Display filesystem UUID
 sudo blkid
-
-
-
 # Create mount directory
 sudo mkdir -p /mnt/mydisk
-
-
-
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
-sudo mount UUID=/dev/sda1: UUID="12345678-abcd-4ef0-9012-3456789abcde" TYPE="ext4" /mnt/mydisk
-
-
-
+sudo mount UUID=/dev/sda1: UUID="12345678-abcd-4ef0-9012-3456789abcde" TYPE="ext4" /mnt/mydi
 # Display mounted filesystem
 df -h /mnt/mydisk
 
